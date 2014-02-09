d0e9860e9eaa9fdcedb2c9914ebef39deca4e9d5
Filipp Frizzy <filipp.s.frizzy@gmail.com>
2014-02-09 05:45:31 +0300

    add delete and modify to group.py; update exists in group.py

--- fabtools/group.py
+++ fabtools/group.py
@@ -9,12 +9,18 @@ from fabric.api import hide, run, settings
 from fabtools.utils import run_as_root
 
 
-def exists(name):
+def exists(name, gid=None):
     """
     Check if a group exists.
     """
-    with settings(hide('running', 'stdout', 'warnings'), warn_only=True):
-        return run('getent group %(name)s' % locals()).succeeded
+    if gid:
+        with settings(hide('running', 'stdout', 'warnings'), warn_only=True):
+            if gid in run('getent group %(name)s' % locals()).split(":"):
+                return True
+            return False
+    else:
+        with settings(hide('running', 'stdout', 'warnings'), warn_only=True):
+            return run('getent group %(name)s' % locals()).succeeded
 
 
 def create(name, gid=None):
@@ -36,3 +42,39 @@ def create(name, gid=None):
     args.append(name)
     args = ' '.join(args)
     run_as_root('groupadd %s' % args)
+
+def modify(name, new_name=None, gid=None):
+        """
+    Modify a group.
+
+    Example::
+
+        import fabtools
+
+        if fabtools.group.exists('admin'):
+            fabtools.group.modify('admin', 'hacker')
+
+    """
+
+    args = []
+    if gid:
+        args.append('-g %s' % gid)
+    if new_name:
+        args.append('-n %s' % new_name)
+    args.append(name)
+    args = ' '.join(args)
+    run_as_root('groupmod %s' % args)
+
+def delete(name):
+        """
+    Delete a group.
+
+    Example::
+
+        import fabtools
+
+        if fabtools.group.exists('admin'):
+            fabtools.group.delete('admin')
+
+    """
+    run_as_root('groupdel %s' % name)
